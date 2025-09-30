c4026403e13976858b69443ef4b1b40ad08e8cc4
RIAZxUDDIN <riaz.uddin.p.3000@gmail.com>
2025-09-30 19:53:01 +0600

    Watermelon 4A solved

---
+++ src/Day1/Watermelon4A_codeforces.java
@@ -0,0 +1,14 @@
+package Day1;
+
+import java.util.Scanner;
+
+public class watermelon_4A_codeforces {
+    public static void main(String[] args) {
+        //problem: https://codeforces.com/problemset/problem/4/A
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.close();
+        if(n%2==0 && n>2) System.out.println("YES");
+        else System.out.println("NO");
+    }
+}
