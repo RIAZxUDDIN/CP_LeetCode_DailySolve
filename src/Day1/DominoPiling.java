aed1d1591dc71156801180e35c195a8ba52fc5d7
RIAZxUDDIN <riaz.uddin.p.3000@gmail.com>
2025-09-30 20:28:11 +0600

    Problem link attached

--- src/Day1/DominoPiling.java
+++ src/Day1/DominoPiling.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 
 public class DominoPiling {
     public static void main(String[] args) {
+        //Problem: https://codeforces.com/problemset/problem/50/A
         Scanner sc = new Scanner(System.in);
         int m,n;
         m = sc.nextInt();
