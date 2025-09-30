1274ee0ca1cfe5da70e74abdf24dff9fe2bab267
RIAZxUDDIN <riaz.uddin.p.3000@gmail.com>
2025-09-30 20:25:48 +0600

    Domino Piling solved

---
+++ src/Day1/DominoPiling.java
@@ -0,0 +1,12 @@
+package Day1;
+import java.util.Scanner;
+
+public class DominoPiling {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m,n;
+        m = sc.nextInt();
+        n = sc.nextInt();
+        System.out.println((m*n)/2);
+    }
+}
